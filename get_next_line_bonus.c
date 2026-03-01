9945f0588f2b887b841f6cb4c170ebd39c2e2750
hM <hoummani.mouna@etu.uae.ac.ma>
2026-03-01 23:31:40 +0000

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -45,7 +45,7 @@ char	*update_res(char *res, int i)
 	return (res);
 }
 
-char	*help(int fd, char *res)
+char	*read_line(int fd, char *res)
 {
 	char	*buff;
 	int		x;
@@ -85,7 +85,7 @@ char	*get_next_line(int fd)
 		return (NULL);
 	if (read(fd, NULL, 0) == -1)
 		return (free(res[fd]), res[fd] = NULL, NULL);
-	res[fd] = help(fd, res[fd]);
+	res[fd] = read_line(fd, res[fd]);
 	if (!res[fd] || res[fd][0] == '\0')
 		return (free(res[fd]), res[fd] = NULL, NULL);
 	i = found_new_line(res[fd]);
